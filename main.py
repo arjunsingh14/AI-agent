5addd3ca1546856aa49eadd76a7f90ea111bbe51
Arjun Bhandal <arjunsingh140601@gmail.com>
2025-11-04 19:36:51 -0500

    Added list of user prompts for history

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 API_KEY = os.environ.get("GEMINI_API_KEY")
+
 
 def main():
     client = genai.Client(api_key=API_KEY)
@@ -13,10 +15,17 @@ def main():
         print("Please enter a prompt")
         exit(1)
     prompt = arg[1]
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
+    messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001", contents=messages
+    )
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    if '--verbose' in sys.argv:
+        print(f"User prompt: {prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
 
 if __name__ == "__main__":
     main()
