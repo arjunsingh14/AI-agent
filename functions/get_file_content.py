c262f8a5a5fb031fbbbab7dd900906103066e26e
Arjun Bhandal <arjunsingh140601@gmail.com>
2025-11-11 17:58:36 -0500

    Gemini can now talk to itself to fix code

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,6 +1,6 @@
 import os
 from google.genai import types
-from config import MAX_CHARS
+from config import CHARACTER_LIMIT
 
 
 def get_file_content(working_directory, file_path):
@@ -12,10 +12,10 @@ def get_file_content(working_directory, file_path):
         return f'Error: File not found or is not a regular file: "{file_path}"'
     try:
         with open(abs_file_path, "r") as f:
-            content = f.read(MAX_CHARS)
-            if os.path.getsize(abs_file_path) > MAX_CHARS:
+            content = f.read(CHARACTER_LIMIT)
+            if os.path.getsize(abs_file_path) > CHARACTER_LIMIT:
                 content += (
-                    f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+                    f'[...File "{file_path}" truncated at {CHARACTER_LIMIT} characters]'
                 )
         return content
     except Exception as e:
@@ -24,7 +24,7 @@ def get_file_content(working_directory, file_path):
 
 schema_get_file_content = types.FunctionDeclaration(
     name="get_file_content",
-    description=f"Reads and returns the first {MAX_CHARS} characters of the content from a specified file within the working directory.",
+    description=f"Reads and returns the first {CHARACTER_LIMIT} characters of the content from a specified file within the working directory.",
     parameters=types.Schema(
         type=types.Type.OBJECT,
         properties={
--- main.py
+++ main.py
@@ -3,71 +3,113 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-from functions.get_files_info import (
-    schema_get_files_info,
-    schema_get_file_content,
-    schema_write_file,
-    schema_run_python_file,
-    get_files_info,
-    get_file_content,
-    write_file,
-    run_python_file,
-)
+from functions.get_files_info import get_files_info, schema_get_files_info
+from functions.get_file_content import get_file_content, schema_get_file_content
+from functions.run_python import run_python_file, schema_run_python_file
+from functions.write_file import write_file, schema_write_file
+from config import MAX_ITERS
 
 load_dotenv()
 API_KEY = os.environ.get("GEMINI_API_KEY")
 
 
-def call_function(function_call, verbose=False):
-    available_functions = {
-        "get_files_info": get_files_info,
-        "get_file_content": get_file_content,
-        "write_file": write_file,
-        "run_python_file": run_python_file,
-    }
-    function_name = function_call.name
-    if verbose:
-        print(f"Calling function: {function_name}({function_call.args})")
-    else:
-        print(f" - Calling function: {function_name}")
-    args = dict(function_call.args)
-    args["working_directory"] = "./calculator"
-    if function_name not in available_functions:
+
+def main():
+    def generate_content(client, messages, verbose):
+        response = client.models.generate_content(
+            model="gemini-2.0-flash-001",
+            contents=messages,
+            config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt
+            ),
+        )
+    
+        if verbose:
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        if response.candidates:
+            for candidate in response.candidates:
+                function_call_content = candidate.content
+                messages.append(function_call_content)
+
+        if not response.function_calls:
+            return response.text
+        
+        function_responses = []
+        for fn in response.function_calls:
+            result = call_function(fn)
+        if not result.parts and  not result.parts[0].function_response:
+            raise Exception("empty function call result")
+        if verbose:
+            print(f"-> {result.parts[0].function_response.response}")
+        function_responses.append(result.parts[0])
+
+        if not function_responses:
+            raise Exception("no function responses generated, exiting")
+        messages.append(types.Content(role="user", parts=function_responses))
+
+    def call_function(function_call, verbose=False):
+        available_functions = {
+            "get_files_info": get_files_info,
+            "get_file_content": get_file_content,
+            "write_file": write_file,
+        "   run_python_file": run_python_file,
+        }
+        function_name = function_call.name
+        if verbose:
+            print(f"Calling function: {function_name}({function_call.args})")
+        else:
+            print(f" - Calling function: {function_name}")
+        args = dict(function_call.args)
+        args["working_directory"] = "./calculator"
+        if function_name not in available_functions:
+            return types.Content(
+                role="tool",
+                parts=[
+                    types.Part.from_function_response(
+                        name=function_name,
+                        response={"error": f"Unknown function: {function_name}"},
+                    )
+                ],
+            )
+        fn = available_functions.get(function_name)
+        result = fn(**args)
         return types.Content(
             role="tool",
             parts=[
                 types.Part.from_function_response(
                     name=function_name,
-                    response={"error": f"Unknown function: {function_name}"},
+                    response={"result": result},
                 )
             ],
-        )
-    fn = available_functions.get(function_name)
-    result = fn(**args)
-    return types.Content(
-        role="tool",
-        parts=[
-            types.Part.from_function_response(
-                name=function_name,
-                response={"result": result},
-            )
-        ],
-    )
-
-
-def main():
+        )        
     system_prompt = """
-You are a helpful AI coding agent.
+You are a helpful AI agent designed to help the user write code within their codebase.
 
-When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+When a user asks a question or makes a request, make a function call plan. For example, if the user asks "what is in the config file in my current directory?", your plan might be:
+
+1. Call a function to list the contents of the working directory.
+2. Locate a file that looks like a config file
+3. Call a function to read the contents of the config file.
+4. Respond with a message containing the contents
+
+You can perform the following operations:
 
 - List files and directories
 - Read file contents
 - Execute Python files with optional arguments
 - Write or overwrite files
 
-All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security.
+
+You are called in a loop, so you'll be able to execute more and more function calls with each message, so just take the next step in your overall plan.
+
+Most of your plans should start by scanning the working directory (`.`) for relevant files and directories. Don't ask me where the code is, go look for it with your list tool.
+
+Execute code (both the tests and the application itself, the tests alone aren't enough) when you're done making modifications to ensure that everything works as expected.
 """
+    load_dotenv()
+    API_KEY = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=API_KEY)
     arg = sys.argv
     verbose = "--verbose" in sys.argv
@@ -84,24 +126,23 @@ All paths you provide should be relative to the working directory. You do not ne
             schema_run_python_file,
         ]
     )
-    response = client.models.generate_content(
-        model="gemini-2.0-flash-001",
-        contents=messages,
-        config=types.GenerateContentConfig(
-            tools=[available_functions], system_instruction=system_prompt
-        ),
-    )
-    for fn in response.function_calls:
-        result = call_function(fn)
-        if not result.parts[0].function_response.response:
-            raise TypeError()
-        if verbose:
-            print(f"-> {result.parts[0].function_response.response}")
 
-    if verbose:
-        print(f"User prompt: {prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    iters = 0
+    while True:
+        iters += 1
+        if iters > MAX_ITERS:
+            print(f"Maximum iterations ({MAX_ITERS}) reached.")
+            sys.exit(1)
+
+        try:
+            final_response = generate_content(client, messages, verbose)
+            if final_response:
+                print("Final response:")
+                print(final_response)
+                break
+        except Exception as e:
+            print(f"Error in generate_content: {e}")
 
 
 if __name__ == "__main__":
